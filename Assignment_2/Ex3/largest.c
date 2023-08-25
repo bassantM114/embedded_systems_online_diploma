2ac16711a70326de85d4be73de18b9d2048ad02a
basant mohamed <117382608+bassantM114@users.noreply.github.com>
2023-08-25 14:26:49 +0300

    Example 3

---
+++ Assignment_2/Ex3/largest.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    float num1, num2, num3;
+    printf("Enter three numbers: ");
+    fflush(stdin);
+    fflush(stdout);
+    scanf("%f %f %f", &num1, &num2, &num3);
+
+    if (num1 > num2 && num1 > num3)
+    {
+        printf("Largest Number = %f", num1);
+    }
+    else if (num2 > num1 && num2 > num3)
+    {
+        printf("Largest Number = %f", num2);
+    }
+    else
+    {
+        printf("Largest Number = %f", num3);
+    }
+}
